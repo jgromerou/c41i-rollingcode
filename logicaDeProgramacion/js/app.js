f1095d3101d1594f715b5b0bbd87c035c876c6aa
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-01-24 15:32:29 -0300

    creando la carpeta logica de programacion

---
+++ logicaDeProgramacion/js/app.js
@@ -0,0 +1,19 @@
+/************Ejercicio 1 **********************/
+console.log('Ejercicio 1');
+console.log(
+  'Leer dos números enteros, presentar por pantalla el promedio de ellos'
+);
+let num1 = 2,
+  num2 = 3;
+let res = num1 + num2;
+let promedio = (num1 + num2) / 2;
+console.log('El promedio es: ' + promedio);
+console.log('****************************************');
+
+/************Ejercicio 2 **********************/
+console.log('Ejercicio 2');
+console.log(
+  'Convertir temperatura en grados Fahrenheit a grados centígrados. Presentar por pantalla ambos valores con sus unidades.    T°C = 5/9 (T°F - 32)'
+);
+
+console.log('****************************************');
